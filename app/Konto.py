0e89c99e7c583afeb56a3004afc757e3be04427e
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-10-21 14:44:50 +0200

    feature_2

--- app/Konto.py
+++ app/Konto.py
@@ -1,6 +1,10 @@
 class Konto:
     def __init__(self, pesel, imie, nazwisko):
-        self.pesel = pesel
         self.imie = imie
         self.nazwisko = nazwisko
         self.saldo = 0
+
+        if (len(pesel) == 11):
+            self.pesel = pesel
+        else:
+            self.pesel = "Niepoprawny pesel!"
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -6,8 +6,8 @@ from ..Konto import Konto
 class TestCreateBankAccount(unittest.TestCase):
 
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto("Dariusz", "Januszewski")
-        self.assertEqual(pierwsze_konto.pesel, "123456789012",
+        pierwsze_konto = Konto("12345678901", "Dariusz", "Januszewski")
+        self.assertEqual(pierwsze_konto.pesel, "12345678901",
                          "Pesel nie został zapisany!")
         self.assertEqual(pierwsze_konto.imie, "Dariusz",
                          "Imie nie zostało zapisane!")
@@ -16,3 +16,8 @@ class TestCreateBankAccount(unittest.TestCase):
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
 
     # tutaj proszę dodawać nowe testy
+
+    def test_when_pesel_is_not_eleven_in_length(self):
+        account = Konto("1", "Dariusz", "Januszkiewicz")
+        self.assertEqual(account.pesel, "Niepoprawny pesel!",
+                         'Pesel nie ma wartości "Niepoprawny pesel", gdy podamy pesel w złym formacie!')
