9a9c82c959fa750233e960766951117d51c08975
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-10-21 14:35:41 +0200

    installed autopref8 and added pesel with tests

--- app/Konto.py
+++ app/Konto.py
@@ -1,5 +1,6 @@
 class Konto:
-    def __init__(self, imie, nazwisko):
+    def __init__(self, pesel, imie, nazwisko):
+        self.pesel = pesel
         self.imie = imie
         self.nazwisko = nazwisko
         self.saldo = 0
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -2,12 +2,17 @@ import unittest
 
 from ..Konto import Konto
 
+
 class TestCreateBankAccount(unittest.TestCase):
 
     def test_tworzenie_konta(self):
         pierwsze_konto = Konto("Dariusz", "Januszewski")
-        self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
-        self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
+        self.assertEqual(pierwsze_konto.pesel, "123456789012",
+                         "Pesel nie został zapisany!")
+        self.assertEqual(pierwsze_konto.imie, "Dariusz",
+                         "Imie nie zostało zapisane!")
+        self.assertEqual(pierwsze_konto.nazwisko, "Januszewski",
+                         "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
 
-    #tutaj proszę dodawać nowe testy
+    # tutaj proszę dodawać nowe testy
