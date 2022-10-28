c6a0ea8d4f4756793b18d68c9b4786de2bbbe405
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-10-28 14:51:00 +0200

    feature_6

--- app/Konto.py
+++ app/Konto.py
@@ -2,24 +2,31 @@ import re
 
 
 class Konto:
-    def __init__(self, pesel, imie, nazwisko, coupon=""):
-        self.imie = imie
-        self.nazwisko = nazwisko
+    def __init__(self, pesel: str, imie: str, nazwisko: str, coupon: str = ""):
+        self.imie: str = imie
+        self.nazwisko: str = nazwisko
 
         self.checkIfPeselIsValid(pesel)
         self.checkIfPromoIsApplicable(coupon)
 
-    def checkIfPeselIsValid(self, pesel):
+    def checkIfPeselIsValid(self, pesel: str):
         if (len(pesel) == 11):
-            self.pesel = pesel
+            self.pesel: str = pesel
         else:
-            self.pesel = "Niepoprawny pesel!"
+            self.pesel: str = "Niepoprawny pesel!"
 
     def checkIfPromoIsApplicable(self, coupon):
         isApplicable = re.search("PROM_", coupon) != None and len(
             coupon) == 8 and (int(self.pesel[2:4]) > 20 or int(self.pesel[0:2]) > 60)
 
         if (isApplicable):
-            self.saldo = 50
+            self.saldo: float = 50
         else:
-            self.saldo = 0
+            self.saldo: float = 0
+
+    def transferIn(self, amount: float):
+        self.saldo += amount
+
+    def transferOut(self, amount: float):
+        if (self.saldo >= amount):
+            self.saldo -= amount
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -4,12 +4,13 @@ from app.Konto import Konto
 
 
 class TestCreateBankAccount(unittest.TestCase):
-    pesel = "12345678901"
     elderlyPesel = "59013456789"
 
     name = "Dariusz"
     surname = "Januszewski"
+    pesel = "12345678901"
     initialSaldo = 0
+
     coupon = "PROM_XYZ"
 
     def test_tworzenie_konta(self):
@@ -54,3 +55,32 @@ class TestCreateBankAccount(unittest.TestCase):
 
         self.assertEqual(account.saldo, 0,
                          "Kupon nie powinien być ważny dla osób urodzonych po 1960!")
+
+
+class TestChangingSaldo(unittest.TestCase):
+
+    name = "Dariusz"
+    surname = "Januszewski"
+    pesel = "12345678901"
+
+    def test_increases_on_transfer_in(self):
+        account = Konto(self.pesel, self.name, self.surname)
+        account.transferIn(100)
+
+        self.assertEqual(
+            account.saldo, 100, "Saldo nie zmienia się poprawnie przy przelewach przychodzących!")
+
+    def test_decreases_on_transfer_out(self):
+        account = Konto(self.pesel, self.name, self.surname)
+        account.saldo = 100
+        account.transferOut(99)
+
+        self.assertEqual(
+            account.saldo, 1, "Saldo nie zmienia się poprawnie przy przelewach przychodzących!")
+
+    def test_transfer_declines_if_transfer_out_is_bigger_than_saldo(self):
+        account = Konto(self.pesel, self.name, self.surname)
+        account.transferOut(100)
+
+        self.assertEqual(
+            account.saldo, 0, "Saldo nie zmienia się poprawnie gdy przelew wychodzący jest większy od aktualnego salda konta!")
