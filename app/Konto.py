8379560cbaf0ae196b954cf4d23753db6eff5385
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-10-28 15:21:38 +0200

    added types to Konto

--- app/Konto.py
+++ app/Konto.py
@@ -2,31 +2,31 @@ import re
 
 
 class Konto:
-    def __init__(self, pesel: str, imie: str, nazwisko: str, coupon: str = ""):
-        self.imie: str = imie
-        self.nazwisko: str = nazwisko
+    def __init__(self, pesel: str, imie: str, nazwisko: str, coupon: str = "") -> None:
+        self.imie = imie
+        self.nazwisko = nazwisko
 
         self.checkIfPeselIsValid(pesel)
         self.checkIfPromoIsApplicable(coupon)
 
-    def checkIfPeselIsValid(self, pesel: str):
+    def checkIfPeselIsValid(self, pesel: str) -> None:
         if (len(pesel) == 11):
-            self.pesel: str = pesel
+            self.pesel = pesel
         else:
-            self.pesel: str = "Niepoprawny pesel!"
+            self.pesel = "Niepoprawny pesel!"
 
-    def checkIfPromoIsApplicable(self, coupon):
-        isApplicable = re.search("PROM_", coupon) != None and len(
+    def checkIfPromoIsApplicable(self, coupon) -> None:
+        isApplicable = coupon[0:5] == "PROM_" != None and len(
             coupon) == 8 and (int(self.pesel[2:4]) > 20 or int(self.pesel[0:2]) > 60)
 
         if (isApplicable):
-            self.saldo: float = 50
+            self.saldo = 50.0
         else:
-            self.saldo: float = 0
+            self.saldo = 0.0
 
-    def transferIn(self, amount: float):
+    def transferIn(self, amount: float) -> None:
         self.saldo += amount
 
-    def transferOut(self, amount: float):
+    def transferOut(self, amount: float) -> None:
         if (self.saldo >= amount):
             self.saldo -= amount
