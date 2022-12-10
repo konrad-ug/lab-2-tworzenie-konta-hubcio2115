7546a8d066254e60d93de3cfedc8e8592078b9bf
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-12-10 13:43:57 +0100

    features for api

--- app/api.py
+++ app/api.py
@@ -1,31 +1,53 @@
 from flask import Flask, request, jsonify
-from app.RejestrKont import RejestrKont
-from app.Konto import Konto
+from app.AccountRegistry import AccountRegistry
+from app.Account import Account
 app = Flask(__name__)
 
 
-@app.route("/konta/stworz_konto", methods=['POST'])
-def stworz_konto():
+@app.route("/accounts/createAccount", methods=['POST'])
+def createAccount():
     dane = request.get_json()
-    if (RejestrKont.searchUser(dane.get('pesel')) != None):
-        return "Konto o podanym numerze PESEL już istnieje", 409
-    if (dane.get('imie') and dane.get('nazwisko') and dane.get('pesel')):
+    if (AccountRegistry.searchUser(dane.get('pesel')) != None):  # type: ignore
+        return "Konto o podanym numerze PESEL już istnieje", 400
+    if (dane.get('name') and dane.get('surname') and dane.get('pesel')):  # type: ignore
         print(f"Request o stworzenie konta z danymi: {dane}")
-        konto = Konto(dane["imie"], dane["nazwisko"], dane["pesel"])
-        RejestrKont.addUser(konto)
+        konto = Account(dane["name"], dane["surname"],  # type: ignore
+                        dane["pesel"])  # type: ignore
+        AccountRegistry.addUser(konto)
         return jsonify("Konto stworzone"), 201
     return "Niepoprawne dane!", 400
 
 
-@app.route("/konta/ile_kont", methods=['GET'])
-def ile_kont():
-    return jsonify(RejestrKont.usersCount()), 200
+@app.route("/accounts/countAccounts", methods=['GET'])
+def countAccounts():
+    return jsonify(AccountRegistry.usersCount()), 200
 
 
-@app.route("/konta/konto/<pesel>", methods=['GET'])
-def wyszukaj_konto_z_peselem(pesel):
-    if (RejestrKont.searchUser(pesel) == None):
+@app.route("/accounts/account/<pesel>", methods=['GET'])
+def searchAccountByPesel(pesel: str):
+    if (AccountRegistry.searchUser(pesel) == None):
         return jsonify("Nie znaleziono konta!"), 404
 
     else:
-        return jsonify(RejestrKont.searchUser(pesel).__dict__), 200
+        return jsonify(AccountRegistry.searchUser(pesel).__dict__), 200
+
+
+@app.route("/konta/konto/<pesel>", methods=['PUT'])
+def update_account(pesel: str):
+    if (AccountRegistry.searchUser(pesel) == None):
+        return jsonify("Nie znaleziono konta!"), 404
+    else:
+        dane = request.get_json()
+        print(dane)
+        AccountRegistry.updateUser(pesel, dane)  # type: ignore
+        return jsonify("Konto zaktualizowane"), 200
+
+
+@app.route("/konta/konto/<pesel>", methods=['DELETE'])
+def delete_account(pesel: str):
+    if (AccountRegistry.searchUser(pesel) == None):
+        return jsonify("Nie znaleziono konta!"), 404
+
+    else:
+        AccountRegistry.deleteUser(pesel)
+        return jsonify("Konto usunięte"), 200
